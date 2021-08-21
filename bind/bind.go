49fe85d0894989c0461b8415e7a113872cb654bc
amirrezaask <raskarpour@gmail.com>
2021-08-22 01:36:45 +0430

    handle multiple return rows in bind

--- bind/bind.go
+++ bind/bind.go
@@ -6,13 +6,7 @@ import (
 	"unsafe"
 )
 
-// Bind binds given rows to the given object at v.
-func Bind(rows *sql.Rows, v interface{}) error {
-	cts, err := rows.ColumnTypes()
-	if err != nil {
-		return err
-	}
-
+func makeScanIntoList(v interface{}, cts []*sql.ColumnType) []interface{} {
 	t := reflect.TypeOf(v)
 
 	vt := reflect.ValueOf(v)
@@ -36,12 +30,45 @@ func Bind(rows *sql.Rows, v interface{}) error {
 			}
 		}
 	}
+	return scanInto
 
-	for rows.Next() {
-		err = rows.Scan(scanInto...)
+}
+
+// Bind binds given rows to the given object at v.
+func Bind(rows *sql.Rows, v interface{}) error {
+	cts, err := rows.ColumnTypes()
+	if err != nil {
+		return err
+	}
+
+	t := reflect.TypeOf(v)
+	vt := reflect.ValueOf(v)
+
+	if t.Kind() == reflect.Ptr {
+		vt = vt.Elem()
+		t = t.Elem()
+	}
+
+	inputs := [][]interface{}{}
+	if t.Kind() != reflect.Slice {
+		inputs = append(inputs, makeScanIntoList(v, cts))
+	} else {
+		for i := 0; i < vt.Len(); i++ {
+			p := vt.Index(i)
+			if p.Type().Kind() == reflect.Ptr {
+				p = p.Elem()
+			}
+			inputs = append(inputs, makeScanIntoList(p.Interface(), cts))
+		}
+	}
+
+	i := 0
+	for rows.Next() && i < len(inputs) {
+		err = rows.Scan(inputs[i]...)
 		if err != nil {
 			return err
 		}
+		i++
 	}
 
 	return nil
--- bind/bind_test.go
+++ bind/bind_test.go
@@ -14,20 +14,61 @@ type User struct {
 }
 
 func TestBind(t *testing.T) {
-	db, err := sql.Open("postgres", "host=127.0.0.1 port=5432 user=connect password=connect dbname=connect sslmode=disable")
-	assert.NoError(t, err)
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(255))")
+	t.Run("single result", func(t *testing.T) {
+		db, err := sql.Open("postgres", "host=127.0.0.1 port=5432 user=connect password=connect dbname=connect sslmode=disable")
+		assert.NoError(t, err)
 
-	assert.NoError(t, err)
-	_, err = db.Exec("INSERT INTO users (name) VALUES ('amirreza')")
+		defer db.Close()
 
-	assert.NoError(t, err)
-	rows, err := db.Query(`SELECT * FROM users`)
-	assert.NoError(t, err)
+		_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(255))")
+		assert.NoError(t, err)
 
-	u := &User{}
-	err = Bind(rows, u)
-	assert.NoError(t, err)
+		_, err = db.Exec("DELETE FROM users")
+		assert.NoError(t, err)
 
-	assert.Equal(t, "amirreza", u.Name)
+		_, err = db.Exec("INSERT INTO users (name) VALUES ('amirreza')")
+		assert.NoError(t, err)
+
+		rows, err := db.Query(`SELECT * FROM users`)
+		assert.NoError(t, err)
+
+		u := &User{}
+
+		err = Bind(rows, u)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "amirreza", u.Name)
+	})
+
+	t.Run("multi result", func(t *testing.T) {
+		db, err := sql.Open("postgres", "host=127.0.0.1 port=5432 user=connect password=connect dbname=connect sslmode=disable")
+		assert.NoError(t, err)
+
+		defer db.Close()
+
+		_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(255))")
+		assert.NoError(t, err)
+
+		_, err = db.Exec("DELETE FROM users")
+		assert.NoError(t, err)
+
+		_, err = db.Exec("INSERT INTO users (name) VALUES ('amirreza')")
+		assert.NoError(t, err)
+
+		_, err = db.Exec("INSERT INTO users (name) VALUES ('milad')")
+		assert.NoError(t, err)
+
+		assert.NoError(t, err)
+		rows, err := db.Query(`SELECT * FROM users`)
+		assert.NoError(t, err)
+
+		amirreza := &User{}
+		milad := &User{}
+
+		err = Bind(rows, []interface{}{amirreza, milad})
+		assert.NoError(t, err)
+
+		assert.Equal(t, "amirreza", amirreza.Name)
+		assert.Equal(t, "milad", milad.Name)
+	})
 }
