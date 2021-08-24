cb688a90e9f1f3dc36d8d72ba535b0d5e58ac3fb
amirrezaask <raskarpour@gmail.com>
2021-08-25 02:54:43 +0430

    add delete stmt

---
+++ builder/delete.go
@@ -0,0 +1,65 @@
+package builder
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"strings"
+)
+
+type deleteStmt struct {
+	table string
+	where *deleteWhere
+}
+
+type deleteWhere struct {
+	parent *deleteStmt
+	conds  []string
+}
+
+func (w *deleteWhere) And(parts ...string) *deleteWhere {
+	w.conds = append(w.conds, "AND "+strings.Join(parts, " "))
+	return w
+}
+
+func (w *deleteWhere) Stmt() *deleteStmt {
+	return w.parent
+}
+func (w *deleteWhere) Or(parts ...string) *deleteWhere {
+	w.conds = append(w.conds, "OR "+strings.Join(parts, " "))
+	return w
+}
+func (w *deleteWhere) Not(parts ...string) *deleteWhere {
+	w.conds = append(w.conds, "NOT "+strings.Join(parts, " "))
+	return w
+}
+
+func (w *deleteWhere) String() string {
+	return strings.Join(w.conds, " ")
+}
+
+func (d *deleteStmt) Where(parts ...string) *deleteWhere {
+	w := &deleteWhere{conds: []string{strings.Join(parts, " ")}, parent: d}
+	d.where = w
+	return w
+}
+
+func (d *deleteStmt) SQL() (string, error) {
+	return fmt.Sprintf("DELETE FROM %s WHERE %s", d.table, d.where.String()), nil
+}
+
+func (d *deleteStmt) ExecContext(ctx context.Context, db *sql.DB, args ...interface{}) (sql.Result, error) {
+	s, err := d.SQL()
+	if err != nil {
+		return nil, err
+	}
+	return exec(context.Background(), db, s, args)
+}
+func (d *deleteStmt) Exec(db *sql.DB, args ...interface{}) (sql.Result, error) {
+	query, err := d.SQL()
+	if err != nil {
+		return nil, err
+	}
+	return exec(context.Background(), db, query, args)
+
+}
