ce5057c4733eda0eaecbcbdd782b5911d870446a
AmirrezaAsk <raskarpour@gmail.com>
2022-04-09 23:59:15 +0430

    remove unnecessary files

--- query.go
+++ query.go
@@ -744,3 +744,56 @@ func Raw(sql string, args ...interface{}) *raw {
 func NewQueryBuilder[OUTPUT any](s *schema) *QueryBuilder[OUTPUT] {
 	return &QueryBuilder[OUTPUT]{schema: s}
 }
+
+type insertStmt struct {
+	PlaceHolderGenerator func(n int) []string
+	Table                string
+	Columns              []string
+	Values               [][]interface{}
+}
+
+func (i insertStmt) flatValues() []interface{} {
+	var values []interface{}
+	for _, row := range i.Values {
+		values = append(values, row...)
+	}
+	return values
+}
+
+func (i insertStmt) getValuesStr() string {
+	phs := i.PlaceHolderGenerator(len(i.Values) * len(i.Values[0]))
+
+	var output []string
+	for _, valueRow := range i.Values {
+		output = append(output, fmt.Sprintf("(%s)", strings.Join(phs[:len(valueRow)], ",")))
+		phs = phs[len(valueRow):]
+	}
+	return strings.Join(output, ",")
+}
+
+func (i insertStmt) ToSql() (string, []interface{}) {
+	base := fmt.Sprintf("INSERT INTO %s (%s) VALUES %s",
+		i.Table,
+		strings.Join(i.Columns, ","),
+		i.getValuesStr(),
+	)
+	return base, i.flatValues()
+}
+
+
+func postgresPlaceholder(n int) []string {
+	output := []string{}
+	for i := 1; i < n+1; i++ {
+		output = append(output, fmt.Sprintf("$%d", i))
+	}
+	return output
+}
+
+func questionMarks(n int) []string {
+	output := []string{}
+	for i := 0; i < n; i++ {
+		output = append(output, "?")
+	}
+
+	return output
+}
--- query_test.go
+++ query_test.go
@@ -203,3 +203,34 @@ func TestDelete(t *testing.T) {
 		assert.EqualValues(t, []interface{}{"2012-01-10"}, args)
 	})
 }
+
+func TestInsert(t *testing.T) {
+	t.Run("insert into multiple rows", func(t *testing.T) {
+		i := insertStmt{}
+		i.Table = "users"
+		i.PlaceHolderGenerator = Dialects.MySQL.PlaceHolderGenerator
+		i.Columns = []string{"name", "age"}
+		i.Values = append(i.Values, []interface{}{"amirreza", 11}, []interface{}{"parsa", 10})
+		s, args := i.ToSql()
+		assert.Equal(t, `INSERT INTO users (name,age) VALUES (?,?),(?,?)`, s)
+		assert.EqualValues(t, []interface{}{"amirreza", 11, "parsa", 10}, args)
+	})
+
+	t.Run("insert into single row", func(t *testing.T) {
+		i := insertStmt{}
+		i.Table = "users"
+		i.PlaceHolderGenerator = Dialects.MySQL.PlaceHolderGenerator
+		i.Columns = []string{"name", "age"}
+		i.Values = append(i.Values, []interface{}{"amirreza", 11})
+		s, args := i.ToSql()
+		assert.Equal(t, `INSERT INTO users (name,age) VALUES (?,?)`, s)
+		assert.Equal(t, []interface{}{"amirreza", 11}, args)
+	})
+}
+
+func TestPostgresPlaceholder(t *testing.T) {
+	t.Run("for 5 it should have 5", func(t *testing.T) {
+		phs := postgresPlaceholder(5)
+		assert.EqualValues(t, []string{"$1", "$2", "$3", "$4", "$5"}, phs)
+	})
+}
